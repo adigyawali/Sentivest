7d389b8069c5b80f72fecaa593526ad09695c493
Aditya Gyawali <gyawaliaditya16@gmail.com>
2025-06-29 18:48:11 -0600

    finsished signup

--- flask-server/app/__init__.py
+++ flask-server/app/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from flask_cors import CORS
 from .routes.home_routes import home_routes
+from .routes.auth import auth_routes 
 
 def create_app():
     app = Flask(__name__)
     CORS(app)
     app.register_blueprint(home_routes)
+    app.register_blueprint(auth_routes)
     return app
